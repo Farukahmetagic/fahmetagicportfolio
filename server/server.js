cfc3bb5cd25c84eb00093a756c89690fe90eb494
Faruk Ahmetagic <farukahmetagic94@gmail.com>
2021-07-08 14:14:55 +0200

    Heroku updates

--- server/server.js
+++ server/server.js
@@ -7,3 +7,12 @@ const port = process.env.PORT || 3001;
 app.listen(port, () => {
     console.log(`Server is running on ${port}`)
 })
+
+app.use(express.static("client/build"));
+
+if(process.env.NODE_ENV === "production"){
+    const path = require('path');
+    app.get('/*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'../client','build','index.html'))
+    })
+}
